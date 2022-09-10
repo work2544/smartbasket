b609335a7e2c9422321d8d97802b85a7e379e0b1
latthaphol laohapiboonrattana <work21_123@hotmail.com>
2022-09-10 22:18:55 +0700

    add amount

--- src/Screens/MainApp/CartScreen/CartScreen.js
+++ src/Screens/MainApp/CartScreen/CartScreen.js
@@ -1,21 +1,28 @@
-import {View, Text,Image,StyleSheet} from 'react-native';
+import {
+  View,
+  Text,
+  Image,
+  StyleSheet,
+  ScrollView,
+  TouchableOpacity,
+} from 'react-native';
 import React, {useEffect, useState} from 'react';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 const CartScreen = ({navigation, route}) => {
   const [product, setProduct] = useState([]);
   const [total, setTotal] = useState(0);
- // const [totaldiscount, setTotalDiscount] = useState();
+  // const [totaldiscount, setTotalDiscount] = useState();
   useEffect(() => {
     const unsubscribe = navigation.addListener('focus', () => {
       fetchProduct();
     });
     return unsubscribe;
   }, [navigation]);
-  useEffect(()=>{
-    getTotal(product)
-  },[product]
-  )
+  useEffect(() => {
+    getTotal(product);
+  }, [product]);
   const fetchProduct = async () => {
     console.log('call fetch');
     let items = await AsyncStorage.getItem('@cartItems');
@@ -36,7 +43,7 @@ const CartScreen = ({navigation, route}) => {
   };
 
   const getTotal = productData => {
-    console.log("run getottal")
+    console.log('run getottal');
     let curtotal = 0;
     if (product) {
       for (let index = 0; index < productData.length; index++) {
@@ -47,42 +54,115 @@ const CartScreen = ({navigation, route}) => {
       setTotal(curtotal);
     }
   };
-  const RenderProduct=((data,index)=>{
-   
+  const RenderProduct = (data, index) => {
     return (
-      <View key={index}>
-        <Text style={{color:'black',fontSize:10}}>{data[0]}</Text>
-         <Image
-          style={styles.logo}
-          source={{uri:data[2]}}
-        />
-        <Text style={{color:'black',fontSize:10}}>{data[1].regularPrice.value}</Text> 
-        <Text style={{color:'red',fontSize:10}}>{data[1].discount.amountOff}</Text> 
-        <Text style={{color:'green',fontSize:10}}>{data[1].finalPrice.value}</Text> 
-      </View>
-    
+      <TouchableOpacity
+        key={index}
+        style={{
+          width: '95%',
+          height: 150,
+          marginVertical: 7,
+          flexDirection: 'row',
+          alignItems: 'center',
+          
+        }}>
+        <View
+          style={{
+            width: '40%',
+            height: '100%',
+            padding: 10,
+            flexDirection: 'row',
+            alignItems: 'center',
+          }}>
+          <Image style={styles.logo} source={{uri: data[2]}} />
+        </View>
+        <View style={{height: '100%', flex: 1, justifyContent: 'space-around'}}>
+          <View>
+            <Text style={styles.textName}>{data[0]}</Text>
+          </View>
+
+          <View>
+            <View style={{flexDirection: 'row'}}>
+              <Text style={styles.CurPrice}>
+                ฿{data[1].finalPrice.value}&nbsp;&nbsp;
+              </Text>
+              <Text style={styles.oldPrice}>{data[1].regularPrice.value}</Text>
+            </View>
+          </View>
+          
+          <View
+            style={{
+              flexDirection: 'row',
+              borderRadius: 100,
+              marginRight: 10,
+              padding: 4,
+              borderWidth: 1,
+              opacity: 0.5,
+              justifyContent: 'space-between',
+              width:'30%'
+            }}>
+            <Ionicons
+              name="remove-circle-outline"
+              style={styles.icontext}></Ionicons>
+            <Text style={{color: 'black', fontSize: 20}}>&nbsp;1&nbsp;</Text>
+            <Ionicons
+              name="add-circle-outline"
+              style={styles.icontext}></Ionicons>
+          </View>
+
+        </View>
+      </TouchableOpacity>
     );
-      })
+  };
 
   return (
-    <View>
-      {product?product.map(RenderProduct):null}
+    <View style={{width: '100%', height: '100%', backgroundColor: 'white'}}>
+      <ScrollView>
+        <View>
+          <Text style={styles.mycartstyle}>My cart</Text>
+          {product ? product.map(RenderProduct) : null}
+        </View>
+      </ScrollView>
+
       {/* <Text style={{color:'black',fontSize:20}}>{totaldiscount}</Text> */}
-      <Text style={{color:'black',fontSize:20}}>{total}</Text>
+      <Text style={{color: 'black', fontSize: 20}}>{total}</Text>
     </View>
   );
 };
 const styles = StyleSheet.create({
-  container: {
-    paddingTop: 50,
-  },
-  tinyLogo: {
-    width: 50,
-    height: 50,
+  mycartstyle: {
+    fontSize: 20,
+    color: 'black',
+    fontWeight: 'bold',
+    paddingTop: 20,
+    paddingLeft: 16,
+    paddingBottom: 10,
   },
   logo: {
-    width: 66,
-    height: 58,
+    height: '100%',
+    width: '50%',
+    alignSelf: 'flex-start',
+    resizeMode: 'contain',
+  },
+  textName: {
+    color: 'black',
+    fontSize: 20,
+    maxWidth: '100%',
+    fontWeight: 'bold',
+  },
+  oldPrice: {
+    textDecorationLine: 'line-through',
+    textDecorationStyle: 'solid',
+    color: 'red',
+    fontSize: 20,
+  },
+  CurPrice: {
+    color: 'green',
+    fontSize: 20,
+  },
+  icontext: {
+    fontSize: 32,
+    color: 'black',
   },
 });
 export default CartScreen;
